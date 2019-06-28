8d63d35b50c4dfd04174face34b3e6e2d7148fb6
David Díaz González <dgonzalez@suse.de>
2019-06-28 17:59:31 +0100

    Fix problems with focus in the chat disabling drag&drop
    As temporal solution, a small zone has been enabled in each area which
    allows to drag and drop it.

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {WidthProvider, Responsive} from 'react-grid-layout';
+import { WidthProvider, Responsive } from 'react-grid-layout';
 
 import Header from '../Header';
 import Sidebar from '../Sidebar';
@@ -13,24 +13,47 @@ import '../../assets/react-resizable.css';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
+const startDragAndDrop = element => {
+  element.classList.add('dragMe');
+};
+
 function Room() {
   const layouts = {};
+  const speakerArea = React.createRef();
+  const participantsArea = React.createRef();
+  const chatArea = React.createRef();
+
   return (
     <div className="Room">
       <Sidebar />
       <Header />
-      <ResponsiveGridLayout className="layout" layouts={layouts}>
+      <ResponsiveGridLayout
+        draggableHandle=".dragMe"
+        className="layout"
+        layouts={layouts}>
         <div
           key="speaker"
-          data-grid={{w: 6, h: 3, x: 0, y: 0, minW: 2, minH: 3}}>
+          ref={speakerArea}
+          data-grid={{ w: 6, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}>
+          <span onMouseDown={() => startDragAndDrop(speakerArea.current)}>
+            X
+          </span>
           <Speaker />
         </div>
         <div
           key="participants"
-          data-grid={{w: 4, h: 3, x: 6, y: 0, minW: 2, minH: 3}}>
+          ref={participantsArea}
+          data-grid={{ w: 4, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}>
+          <span onMouseDown={() => startDragAndDrop(participantsArea.current)}>
+            X
+          </span>
           <Participants />
         </div>
-        <div key="chat" data-grid={{w: 12, h: 3, x: 0, y: 3, minW: 2, minH: 3}}>
+        <div
+          key="chat"
+          ref={chatArea}
+          data-grid={{ w: 12, h: 3, x: 0, y: 3, minW: 2, minH: 3 }}>
+          <span onMouseDown={() => startDragAndDrop(chatArea.current)}>X</span>
           <Chat />
         </div>
       </ResponsiveGridLayout>
